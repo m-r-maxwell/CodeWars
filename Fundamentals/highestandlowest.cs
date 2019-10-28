678ec07d682d63170e3ea741e6161da1508eb044
Matt Maxwell <mrmaxwell78@gmail.com>
2019-10-28 12:35:44 -0400

    Highest and Lowest

---
+++ Fundamentals/highestandlowest.cs
@@ -0,0 +1,18 @@
+using System;
+
+public static class Kata
+{
+  public static string HighAndLow(string numbers)
+  {
+    var min = int.MaxValue;
+    var max = int.MinValue;
+    foreach (var number in numbers.Split(' '))
+    {
+      var n = int.Parse(number);
+      min = Math.Min(min, n);
+      max = Math.Max(max, n);
+    }
+    return max + " " + min;
+  }
+}//var parsed = numbers.Split().Select(int.Parse);
+ //return parsed.Max() + " " + parsed.Min();
