2ef5f129d2d3adcbb3920cba34467270f04c4314
Matt Maxwell <mrmaxwell78@gmail.com>
2019-11-08 15:28:12 -0500

    twosum js and cs

---
+++ Fundamentals/CSharp/twosum.cs
@@ -0,0 +1,13 @@
+using System;
+public class Kata
+{
+  public static int[] TwoSum(int[] numbers, int target)
+  {
+     for (int i = 0; i < numbers.Length; i++)
+         for (int j = i + 1; j < numbers.Length; j++)
+             if (numbers[i] + numbers[j] == target)
+                return new int[] { i, j };
+                
+     return new int[0];
+  }
+}
