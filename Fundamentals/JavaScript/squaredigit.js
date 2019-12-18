4fa34f39a8efa8c010ae34226fd4026a7b63686d
Matt Maxwell <mrmaxwell78@gmail.com>
2019-12-18 15:17:54 -0500

    Updated with oneliner

--- Fundamentals/JavaScript/squaredigit.js
+++ Fundamentals/JavaScript/squaredigit.js
@@ -1,8 +1,14 @@
 squareDigits = (num) => {
-      let numstring = num.toString();
-      let arr = [];
-      for (var i = 0; i < numstring.length; i++){
-          arr[i] = numstring[i] * numstring[i];
-      }
-      return Number(arr.join(''));
-  }
+    let numstring = num.toString();
+    let arr = [];
+    for (var i = 0; i < numstring.length; i++) {
+        arr[i] = numstring[i] * numstring[i];
+    }
+    return Number(arr.join(''));
+}
+
+/*
+function squareDigits(num){
+  return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+}
+*/
